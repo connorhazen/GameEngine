c2f29a0daa4ef79fb19db35e2f99d097488e98f5
Charles Papandreou <cnp20@duke.edu>
2020-04-09 18:37:26 -0400

    added constructor that uses reflection in helper method

--- src/engine/GridEvent.java
+++ src/engine/GridEvent.java
@@ -1,11 +1,28 @@
 package engine;
 
+import engine.interaction.Interaction;
+import engine.operation.Operation;
+import engine.rules.Rules;
 import util.Grid;
 import util.UserAction;
 
+import java.lang.reflect.Constructor;
+
 public class GridEvent implements Event {
 
+    public static final String OUTTER_PACKAGE = "engine.";
+    public static final String OPERATION_PACKAGE = OUTTER_PACKAGE+"operation.";
+    public static final String INTERACTION_PACKAGE = OUTTER_PACKAGE+"interaction.";
+    public static final String RULES_PACKAGE = OUTTER_PACKAGE+"rules.";
+
+    private Operation myOperation;
+    private Interaction myInteraction;
+    private Rules myRules;
+
     public GridEvent(String operation, String interaction, String rules) {
+        myOperation = (Operation)createObjectOf(operation, OPERATION_PACKAGE);
+        myInteraction = (Interaction)createObjectOf(interaction, INTERACTION_PACKAGE);
+        myRules = (Rules)createObjectOf(rules, RULES_PACKAGE);
     }
 
     @Override
@@ -16,5 +33,19 @@ public class GridEvent implements Event {
     @Override
     public UserAction getNextAction() {
         return null;
+    }
+
+    private Object createObjectOf(String rules, String pack) {
+        Object obj = null;
+        try {
+            Class c = Class.forName(pack + rules);
+            Constructor objConstruct = c.getDeclaredConstructor();
+            objConstruct.setAccessible(true);
+            obj = objConstruct.newInstance();
+        } catch (Exception e) {
+            //TODO: Class you were trying to create had a problem make exception
+            System.out.println("Class you were trying to create had a problem");
+        }
+        return obj;
     }
 }
