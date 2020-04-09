1c7a2905f23f6d0a201c983c45c15b2a8fc9c146
Charles Papandreou <cnp20@duke.edu>
2020-04-09 15:51:47 -0400

    Added required methods and constructor with fields

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,4 +1,33 @@
 package engine;
 
-public class Engine {
+import util.Grid;
+
+public class Engine implements EngineAPI{
+    private Grid currentGrid;
+    private Grid updatedGrid;
+    private GameHandler currGameHandler;
+    private AnimationHandler currAnimationHandler;
+    private LevelHandler currLevelHandler;
+
+    public Engine(GameHandler gameHandler, AnimationHandler animationHandler, LevelHandler levelHandler){
+        currGameHandler = gameHandler;
+        currAnimationHandler = animationHandler;
+        currLevelHandler = levelHandler;
+        currentGrid = currLevelHandler.initializeGrid();
+        updatedGrid = null;
+    }
+
+    @Override
+    public UpdateObject getGrid() {
+        return generateUpdateStatus();
+    }
+
+    @Override
+    public UpdateObject executeEvent(Event e) {
+        return generateUpdateStatus();
+    }
+
+    private UpdateObject generateUpdateStatus() {
+        return null;
+    }
 }
