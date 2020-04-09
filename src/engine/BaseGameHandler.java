e0a5b9912066c122b26a8b17a5c49a71a7be898a
Charles Papandreou <cnp20@duke.edu>
2020-04-09 17:23:36 -0400

    Implemented required game handler methods

--- src/engine/BaseGameHandler.java
+++ src/engine/BaseGameHandler.java
@@ -1,4 +1,38 @@
 package engine;
 
-public class BaseGameHandler {
+import util.Grid;
+import util.UserAction;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseGameHandler implements GameHandler {
+
+    private Map<UserAction,Event> eventMappings;
+    private UserAction nextEvent;
+
+    public BaseGameHandler(){
+        eventMappings = new HashMap<>();
+        nextEvent = null;
+    }
+
+    @Override
+    public void addEvent(UserAction action, String operation, String interaction, String rules){
+        eventMappings.put(action, new GridEvent(operation,interaction,rules));
+    }
+
+    @Override
+    public UserAction getNextEvent() {
+        UserAction retVal = nextEvent;
+        nextEvent = null;
+        return retVal;
+    }
+
+    @Override
+    public Grid generateUpdatedGrid(Grid currentGrid, UserAction action) {
+        Event eventToPerform = eventMappings.get(action);
+        Grid updatedGrid = eventToPerform.execute(currentGrid);
+        nextEvent = eventToPerform.getNextAction();
+        return updatedGrid;
+    }
 }
--- src/engine/GridEvent.java
+++ src/engine/GridEvent.java
@@ -1,6 +1,7 @@
 package engine;
 
 public class GameEvent implements Event {
+
     public GameEvent(String operation, String interaction, String rules) {
     }
 }
