29131214aa32df14505ee2590ec8f18534a2f443
cch57 <christopher.hazen@duke.edu>
2020-04-09 13:35:50 -0600

    extended javafx application

--- src/SimplePlayer.java
+++ src/SimplePlayer.java
@@ -1,3 +1,15 @@
-public class SimplePlayer {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class SimplePlayer extends Player {
+
+
+  public SimplePlayer(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+
+  }
 }
