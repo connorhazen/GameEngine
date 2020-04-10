45ea4769decba183e24efb4b6e847514650c0335
Charles Papandreou <cnp20@duke.edu>
2020-04-09 22:39:10 -0400

    Added abstract class for setting up Interactions

--- src/engine/interaction/OrderedAll.java
+++ src/engine/interaction/OrderedAll.java
@@ -1,4 +1,30 @@
 package engine.interaction;
 
-public class OrderedAll {
+import util.Grid2dArray;
+import util.MutableCell;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class OrderedAll implements Interaction {
+
+    protected List<MutableCell> cells;
+    public abstract void setGrid(Grid2dArray currentGrid);
+
+    public OrderedAll(){
+        cells = new ArrayList<>();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cells != null && cells.size() >= 2;
+    }
+
+    @Override
+    public List<MutableCell> next() {
+        List<MutableCell> pairOfCells = new ArrayList<>();
+        pairOfCells.add(cells.remove(0));
+        pairOfCells.add(cells.remove(0));
+        return pairOfCells;
+    }
 }
