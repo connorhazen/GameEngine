963a49936a41436c85b05c8514b97d4c1e259dfa
Charles Papandreou <cnp20@duke.edu>
2020-04-09 22:54:26 -0400

    started working on combine operation for 2048

--- src/engine/operation/OperationCombine2048.java
+++ src/engine/operation/OperationCombine2048.java
@@ -1,4 +1,20 @@
 package engine.operation;
 
-public class OperationCombine2048 {
+import util.MutableCell;
+import util.SimpleState;
+import util.State;
+
+import java.util.List;
+
+public class OperationCombine2048 implements Operation{
+    @Override
+    public void execute(List<MutableCell> currCells) {
+        MutableCell cell1 = currCells.get(0);
+        MutableCell cell2 = currCells.get(1);
+        State combinedState1 = null;
+        State combinedState2 = null;
+        cell1.setState(combinedState1);
+        cell2.setState(combinedState2);
+
+    }
 }
