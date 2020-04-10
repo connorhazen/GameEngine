d3a69571670a39eab7860af5ff6fc3c74709c39d
Charles Papandreou <cnp20@duke.edu>
2020-04-09 23:09:58 -0400

    Modified operation to handle updating states of cells

--- src/engine/operation/OperationCombine2048.java
+++ src/engine/operation/OperationCombine2048.java
@@ -11,10 +11,9 @@ public class OperationCombine2048 implements Operation{
     public void execute(List<MutableCell> currCells) {
         MutableCell cell1 = currCells.get(0);
         MutableCell cell2 = currCells.get(1);
-        State combinedState1 = null;
-        State combinedState2 = null;
+        State combinedState1 = new SimpleState("",cell1.getValue() + cell2.getValue());
+        State combinedState2 = new SimpleState("",0);
         cell1.setState(combinedState1);
         cell2.setState(combinedState2);
-
     }
 }
