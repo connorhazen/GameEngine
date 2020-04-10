e9dfda7467c4a1a6c9420642d42c71f73588527d
Charles Papandreou <cnp20@duke.edu>
2020-04-09 20:59:58 -0400

    modified interactions operations and rules

--- src/engine/interaction/Interaction.java
+++ src/engine/interaction/Interaction.java
@@ -1,4 +1,15 @@
 package engine.interaction;
 
+import util.Grid;
+import util.MutableCell;
+
+import java.util.List;
+
 public interface Interaction extends Iterable {
+
+    void setGrid(Grid currentGrid);
+
+    boolean hasNext();
+
+    List<MutableCell> getNext();
 }
