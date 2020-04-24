5e4d4f4abf84743a5ef72f4c480f8ce77c6e0c3d
cch57 <christopher.hazen@duke.edu>
2020-04-24 14:45:25 -0600

    added new minsweeper level

--- src/minesweeeperLevelMaker/LevelMaker.java
+++ src/minesweeeperLevelMaker/LevelMaker.java
@@ -4,17 +4,21 @@ import game.controller.GameStorageHandler;
 import game.engine.UpdateStatus;
 import game.util.Coordinates;
 import game.util.Grid2dArray;
+import game.util.MutableCell;
+import game.util.MutableGrid;
 import game.util.SimpleState;
 import java.util.function.Consumer;
 
 public class Main {
   public static void main(String[]args){
-    makeLevel(10, 10);
+    makeLevel(10, 10, .2);
   }
 
-  private static void makeLevel(int width, int height) {
+  private static void makeLevel(int width, int height, double prob) {
     Grid2dArray g = new Grid2dArray(width, height);
     Consumer<Coordinates> c = (e) -> g.setCell(e.x, e.y, new SimpleState("COVERED", 0));
+
+    makeBombs(prob, g);
 
     g.loop(c);
 
@@ -25,6 +29,11 @@ public class Main {
 
 
     GameStorageHandler.storeGame(new UpdateStatus(g), "data/Games/Minesweeper/level1.sav");
+
+
+  }
+  private void makeBombs(double prob, MutableGrid g){
+
   }
 
 }
