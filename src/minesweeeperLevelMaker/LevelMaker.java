5ccfb8956bffd3be21ef6c92e958df2d3bc6bd1c
cch57 <christopher.hazen@duke.edu>
2020-04-24 14:45:54 -0600

    fixing level maker

--- src/minesweeeperLevelMaker/LevelMaker.java
+++ src/minesweeeperLevelMaker/LevelMaker.java
@@ -2,37 +2,67 @@ package minesweeeperLevelMaker;
 
 import game.controller.GameStorageHandler;
 import game.engine.UpdateStatus;
+import game.util.Cell;
 import game.util.Coordinates;
 import game.util.Grid2dArray;
 import game.util.MutableCell;
 import game.util.MutableGrid;
 import game.util.SimpleState;
+import game.util.State;
+import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
 
-public class Main {
+public class LevelMaker {
   public static void main(String[]args){
-    makeLevel(10, 10, .2);
+
+    LevelMaker lm = new LevelMaker();
+    lm.makeLevel(10, 10, .2);
   }
 
-  private static void makeLevel(int width, int height, double prob) {
+  private  void makeLevel(int width, int height, double prob) {
     Grid2dArray g = new Grid2dArray(width, height);
     Consumer<Coordinates> c = (e) -> g.setCell(e.x, e.y, new SimpleState("COVERED", 0));
 
-    makeBombs(prob, g);
-
     g.loop(c);
 
-    g.setCell(0,0, new SimpleState("COVERED", -1));
-    g.setCell(1,0, new SimpleState("COVERED", 1));
-    g.setCell(0,1, new SimpleState("COVERED", 1));
-    g.setCell(1,1, new SimpleState("COVERED", 1));
-
+    makeBombs(prob, g);
+    makeIndicators(g);
 
     GameStorageHandler.storeGame(new UpdateStatus(g), "data/Games/Minesweeper/level1.sav");
 
 
   }
+
+  private void makeIndicators(Grid2dArray g) {
+    Consumer<Coordinates> t = (e) -> {
+      MutableCell current = g.getMutableCell(e);
+      if(current.getValue() != -1 ){
+        List<MutableCell> neighs = g.getNeighbors(current);
+        int counter =0;
+        for(Cell c: neighs){
+          if(c.getValue()==-1){
+            counter++;
+          }
+        }
+        current.setState(new SimpleState(current.getType(), counter));
+      }
+
+    };
+
+    g.loop(t);
+
+  }
+
   private void makeBombs(double prob, MutableGrid g){
+    Consumer<Coordinates> t = (e) -> {
+      if (Math.random() < prob) {
+        State current = g.getMutableCell(e).getState();
+        g.getMutableCell(e).setState(new SimpleState(current.getType(), -1));
+      }
+    };
+
+    g.loop(t);
 
   }
 
