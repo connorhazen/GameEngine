55b5db5aadf0e7425021fd6603d809538c44019c
Charles Papandreou <cnp20@duke.edu>
2020-04-25 02:22:35 -0400

    Added test for ensuring xml parser creates instance of a grid object in engine

---
+++ test/engine/TestEngineCreation.java
@@ -0,0 +1,29 @@
+package engine;
+import game.controller.EngineFactory;
+import game.controller.GameObject;
+import game.engine.Engine;
+import game.engine.animationHandlers.AnimationHandler;
+import game.engine.animationHandlers.BaseAnimationHandler;
+import game.engine.gameHandlers.BaseGameHandler;
+import game.engine.gameHandlers.GameHandler;
+import game.engine.levelHandlers.BaseLevelHandler;
+import game.engine.levelHandlers.LevelHandler;
+import game.util.Coordinates;
+import game.util.Grid;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestEngineCreation {
+
+    @Test
+    void testEngineGridCreation(){
+        GameHandler gh = new BaseGameHandler();
+        AnimationHandler ah = new BaseAnimationHandler();
+        LevelHandler lh = new BaseLevelHandler();
+        EngineFactory.parseXML("Games/Game2048/","engine.xml",gh,ah,lh);
+        Engine e = new Engine(gh,ah,lh);
+        assertTrue(e.getGrid().getGrid() instanceof Grid);
+    }
+}
